51f0c45a0fcae54970884d33ecb6e2662c0fb529
FarhanSadeek21 <farhansadeek019@gmail.com>
2023-11-09 20:43:13 -0500

    save files

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.express as px
-import pandas as pd
 import os
 import warnings
 import streamlit as st
@@ -8,18 +7,30 @@ import numpy as np
 
 warnings.filterwarnings("ignore")
 
-st.set_page_config(page_title='Superstore Sales Dasboard', page_icon=":bar_chart", layout = "wide")
+st.set_page_config(page_title='Superstore Sales Dashboard', page_icon=":bar_chart", layout="wide")
 
 st.title(':bar_chart: SuperStore Exploratory Data Analysis')
-st.markdown('<style>div.block-container{padding-top: 1 rem;}</style>', unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top: 1rem;}</style>', unsafe_allow_html=True)
 
-file = st.file_uploader(":file_folder: Upload your file ", type=['csv'])
+file = st.file_uploader(":file_folder: Upload your file", type=['csv'])
+custom_name = st.text_input("Enter a custom name for the file (optional):")
 
 if file is not None:
-    file_name = file.name
-    st.write(file_name)
+    if custom_name:
+        file_name = custom_name + ".csv"
+    else:
+        file_name = file.name
+
+    st.write(f"File Name: {file_name}")
+
+    # Save the uploaded file
+    with open(file_name, "wb") as f:
+        f.write(file.getbuffer())
+
+    st.success(f"File saved successfully: {file_name}")
+
+    # Read the uploaded file into a DataFrame
     df = pd.read_csv(file_name)
 else:
-    current_directory = os.getcwd()
-    os.chdir(current_directory)
-    df = pd.read_excel('Superstore.xls')
+    df = pd.read_csv('Superstore.csv')
+
