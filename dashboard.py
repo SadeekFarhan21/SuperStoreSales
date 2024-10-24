dbe830818252eea512c27dc054b5b88e7f56d5c8
Farhan Sadeek <farhansadeek019@gmail.com>
2024-10-24 22:11:58 +0000

    Changed the name of the project

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import streamlit as st
 import numpy as np
 
 
-st.set_page_config(page_title='Superstore Sales Dashboard', page_icon=":bar_chart", layout="wide")
+st.set_page_config(page_title='RetailIQ', page_icon=":bar_chart", layout="wide")
 
 st.title(':bar_chart: SuperStore Exploratory Data Analysis')
 st.markdown('<style>div.block-container{padding-top: 1rem;}</style>', unsafe_allow_html=True)
